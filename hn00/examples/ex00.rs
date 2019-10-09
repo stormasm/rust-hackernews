8657703b9758faaeb334bf6b372b5aa82d80ae37
Michael Angerman <stormasm@gmail.com>
2019-10-09 11:57:44 -0700

    reading a file and parsing json correctly

--- hn00/examples/ex00.rs
+++ hn00/examples/ex00.rs
@@ -3,9 +3,20 @@ use std::fs;
 use std::process;
 use std::string::String;
 
+use serde_json::{Result, Value};
+
 fn read_file_to_string(filename: String) -> String {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     contents
+}
+
+fn string_to_json(data: String) -> Result<()> {
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(&data)?;
+
+    println!("{}", v[0]);
+
+    Ok(())
 }
 
 fn main() {
@@ -17,7 +28,8 @@ fn main() {
     let filename = &args[1];
     println!("In file {}", filename);
 
-    let contents = read_file_to_string(filename.to_string());
+    let data = read_file_to_string(filename.to_string());
+    println!("With text:\n{}", data);
 
-    println!("With text:\n{}", contents);
+    string_to_json(data);
 }
