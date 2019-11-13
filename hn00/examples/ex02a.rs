ec5a4fbc0a09b6835b4b000b1d062b545eafac7b
Michael Angerman <stormasm@gmail.com>
2019-11-12 21:54:09 -0500

    ex02a

---
+++ hn00/examples/ex02a.rs
@@ -0,0 +1,31 @@
+#![deny(warnings)]
+
+fn build_url(favid: &str) -> String {
+    let mut s = String::from("https://hacker-news.firebaseio.com/v0/item/");
+
+    s.push_str(favid);
+    s.push_str(".json");
+    s
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let url = build_url("8863");
+
+    println!("{}", url);
+
+    /*
+
+        let res = reqwest::Client::new()
+            .get("https://hacker-news.firebaseio.com/v0/item/8863.json")
+            .send()
+            .await?;
+
+        println!("Status: {}", res.status());
+
+        let body = res.text().await?;
+
+        println!("Body:\n\n{}", body);
+    */
+    Ok(())
+}
