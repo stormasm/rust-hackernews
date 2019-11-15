74a0dbc8f259602af7e9608997b51769438a4923
Michael Angerman <stormasm@gmail.com>
2019-11-14 20:04:33 -0500

    passing the ids through to the methods

--- hn00/examples/ex02b.rs
+++ hn00/examples/ex02b.rs
@@ -1,6 +1,5 @@
 fn build_url(favid: &str) -> String {
     let mut s = String::from("https://hacker-news.firebaseio.com/v0/item/");
-
     s.push_str(favid);
     s.push_str(".json");
     s
@@ -14,22 +13,18 @@ async fn get_body(url: String) -> Result<(String), reqwest::Error> {
         .expect("Problem 1");
 
     println!("Status: {}", res.status());
-
     let body = res.text().await.expect("Problem 2");
-
     println!("Body:\n{}", body);
-
     Ok(body)
 }
 
-fn write_redis(body: String) -> redis::RedisResult<()> {
+fn write_redis(body: String, id: &str) -> redis::RedisResult<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
-    // let mut con = client.get_connection()?;
     let mut con = client.get_connection().expect("Failed to connect to Redis");
 
     let _x3 = redis::cmd("HSET")
         .arg("hnfav11-14")
-        .arg("8863")
+        .arg(id)
         .arg(body)
         .query::<u64>(&mut con)
         .unwrap();
@@ -39,19 +34,14 @@ fn write_redis(body: String) -> redis::RedisResult<()> {
 
 async fn gorun(id: &str) -> Result<(), reqwest::Error> {
     let url = build_url(id);
-
     println!("{}", url);
-
     let body = get_body(url).await?;
-
-    let _ = write_redis(body);
-
+    let _ = write_redis(body, id);
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let _ = gorun("8863").await?;
-
     Ok(())
 }
